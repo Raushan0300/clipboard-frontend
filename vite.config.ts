d91cedca7ef96678e2064a4b7f13579e48ad4657
Raushan Kumar <raushankumar2909@gmail.com>
2024-06-11 23:02:33 +0530

    chore: Update dependencies and styles in package.json, Config.tsx, and Home.css

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server:{
+    host:true
+  },
+  preview:{
+    port: parseInt(process.env.PORT ?? '') || 3000
+  }
 })
