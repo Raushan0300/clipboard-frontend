ce4532b48e362ae0465f0e8818dbf039eb803762
Raushan Kumar <raushankumar2909@gmail.com>
2024-06-13 16:14:53 +0530

    chore: Refactor API fetch in Home.tsx to use root endpoint

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -24,7 +24,7 @@ const Home = () => {
 
   useEffect(()=>{
     const fetchAPI = async()=>{
-      await getData("/");
+      await getData("");
       console.log("API is working");
     };
     fetchAPI();
